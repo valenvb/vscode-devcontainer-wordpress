98edeee8306b6866f0c0729c2518cbe00a6dd082
Valen Varangu-Booth <valen@valenvb.ca>
2020-04-04 22:07:09 -0400

    install wordpress

--- .devcontainer/wp-setup.sh
+++ .devcontainer/wp-setup.sh
@@ -1,2 +1,9 @@
 #!  /bin/bash
-echo "hello"
+SITE_TITLE="Dev Site"
+ADMIN_USER=admin
+ADMIN_PASS=password
+ADMIN_EMAIL="admin@localhost.com"
+
+cd /var/www/html;
+#wp core download;
+wp core install --url="localhost:8080" --title="$SITE_TITLE" --admin_user="$ADMIN_USER" --admin_email="$ADMIN_EMAIL" --admin_password="$ADMIN_PASS" --skip-email;
