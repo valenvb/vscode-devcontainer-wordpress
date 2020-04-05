5f8617b45442197d9fa853791291686315df61a4
Valen Varangu-Booth <valen@valenvb.ca>
2020-04-04 22:37:33 -0400

    configure and install wordpress (once)

--- .devcontainer/wp-setup.sh
+++ .devcontainer/wp-setup.sh
@@ -5,5 +5,7 @@ ADMIN_PASS=password
 ADMIN_EMAIL="admin@localhost.com"
 
 cd /var/www/html;
-#wp core download;
-wp core install --url="localhost:8080" --title="$SITE_TITLE" --admin_user="$ADMIN_USER" --admin_email="$ADMIN_EMAIL" --admin_password="$ADMIN_PASS" --skip-email;
+if [ ! -f wp-config.php ]; then 
+    wp config create --dbhost="db" --dbname="wordpress" --dbuser="wp_user" --dbpass="wp_pass" --skip-check
+    wp core install --url="http://localhost:8080" --title="$SITE_TITLE" --admin_user="$ADMIN_USER" --admin_email="$ADMIN_EMAIL" --admin_password="$ADMIN_PASS" --skip-email;
+fi
